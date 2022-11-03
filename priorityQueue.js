26d1dfe52c48ec51e13b2aec35979a1ec92d7566
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-11-03 22:46:51 +0100

    Priority queue

--- priorityQueue.js
+++ priorityQueue.js
@@ -5,58 +5,48 @@ class PriorityQueue{
     constructor(priority = PriorityQueue.ASCENDING){
         this.queue = new LinkedList();
         this.priority = priority;
+        this.size = this.queue.size;
     }
 
     enqueue(val){
         const node = new Node(val);
         let curr = this.queue.head;
-        if (!curr){
-            this.queue.head = node;
-            return
+        if(!curr){
+            this.queue.addNode(val)
+            this.size = this.queue.size;
+            return;
         }
-        if (this.priority === PriorityQueue.ASCENDING){
-            this._ascending(node)
-        } else if (this.priority === PriorityQueue.DESCENDING){
-            this._descending(node)
-        }else{
-            return 'Wrong priority choice!!!'
-        }
-
+    
+            if (this.priority == PriorityQueue.ASCENDING){
+                for (let i=0; i<this.size; i++){
+                    if(curr.element >= node.element){
+                        this.queue.addNodeAt(val, i);
+                        this.size = this.queue.size;
+                        return;
+                    }
+                    curr = curr.next;
+                }
+                this.queue.addNode(val);
+                this.size = this.queue.size;
+            } else if (this.priority == PriorityQueue.DESCENDING){
+                for(let i=0; i<this.size; i++){
+                    if(curr.element <= node.element){
+                        this.queue.addNodeAt(val, i);
+                        this.size = this.queue.size;
+                        return;
+                    }
+                    curr = curr.next;      
+                }
+                this.queue.addNode(val);
+                this.size = this.queue.size;
+            }
         
-    }
-    _ascending(node){
-        let curr = this.queue.head;
-        for (let i=0; i<this.queue.size-1; i++){
-            if (curr.next.element >= node.element){
-                this.queue.addNodeAt(node.element, i);
-                return;
-            }
-            curr = curr.next;
-        }
-        curr.next = node;
-
-    }
-
-    _descending(node){
-        let curr = this.queue.head;
-        for (let i=0; i<this.queue.size-1; i++){
-            if (curr.next.element <= node.element){
-                this.queue.addNodeAt(node.element, i);
-                return;
-            }
-            curr = curr.next;
-        }
-        curr.next = node;
     }
 
     getNodes(){
         return this.queue.listAllNodes();
     }
-
-    }
+}
 
 PriorityQueue.ASCENDING = Symbol('Ascending order');
 PriorityQueue.DESCENDING = Symbol('Descending order');
-const theQueue = new PriorityQueue();
-theQueue.enqueue(45);
-theQueue.enqueue(67);
